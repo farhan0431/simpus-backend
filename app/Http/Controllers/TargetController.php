c2a3013d1577f0f0e6ad3bcd6952e84fab4b55b9
Farhan <farhan@Farhan-MacBook-Air.local>
2021-02-26 16:38:02 +0800

    UPDATE

--- app/Http/Controllers/TargetController.php
+++ app/Http/Controllers/TargetController.php
@@ -23,8 +23,11 @@ class TargetController extends Controller
 
     public function index()
     {
+        
+
+
         $target = TargetPenerimaanSppt::orderBy('tahun','DESC')->when(request()->q, function($query) {
-            $query->where('tahun','LIKE','%'.request()->q.'%');
+            $query->where('tahun','LIKE','%'.request()->q.'%')->orWhere('bulan','LIKE','%'.request()->q.'%')->orWhere('target','LIKE','%'.request()->q.'%');
         })
         ->paginate(10);
         return response()->json(['status' => 'success', 'data' => $target]);
--- app/TargetPenerimaanSppt.php
+++ app/TargetPenerimaanSppt.php
@@ -10,5 +10,28 @@ class TargetPenerimaanSppt extends Model
 
     protected $table = 'target_penerimaan_sppt';
     protected $guarded = [];
+    protected $appends = [
+        'nama_bulan'
+    ];
+
+    public function getNamaBulanAttribute()
+    {
+        $bulan = [
+            '01' => 'Januari',
+            '02' => 'Februari',
+            '03' => 'Maret',
+            '04' => 'April',
+            '05' => 'Mei',
+            '06' => 'Juni',
+            '07' => 'Juli',
+            '08' => 'Agustus',
+            '09' => 'September',
+            '10' => 'Oktober',
+            '11' => 'November',
+            '12' => 'Desember'        
+        ];
+
+        return $bulan[$this->bulan];
+    }
 
 }
